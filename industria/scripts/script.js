0bde90d111c152a288222742b15cd5ca4f32b9f2
Vitor Muneretto Tinelli <vitor.mtinelli@gmail.com>
2023-06-14 15:23:35 -0300

    menu

---
+++ industria/scripts/script.js
@@ -0,0 +1,32 @@
+function redGit(){
+    window.location.href = "https://www.github.com/VitorTinelli";
+}
+
+function redInsta(){
+    window.location.href = "https://www.instagram.com/VitorTinelli";
+}
+
+function getClick(){
+    if (a==1){
+        window.location.href = "funcionarios.php";
+    }
+    else if (a==2){
+        window.location.href = "pedidos.php";
+    }
+    else if (a==3){
+        window.location.href = "faturamento.php";
+    }
+    else if (a==4){
+        window.location.href = "clientes.php";
+    }
+    else if (a==5){
+        window.location.href = "estoque.php";
+    }
+    else if (a==6){
+        window.location.href = "comissao.php";
+    }
+    else if (a==7){
+        window.location.href = "produtos.php";
+    }
+}
+
